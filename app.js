04bedddb8636c19df69582ce8706c548b7f6b007
Umair Sultan <umairsultan703@gmail.com>
2025-08-30 09:37:33 +0500

    Project done

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
+let baseURL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies"
 
+// select the form container
+
+let formContainer = document.querySelector(".form-container");
 
 // select user input 
 
@@ -16,7 +20,11 @@ let result = document.querySelector("#result");
 
 let submit = document.querySelector("#submit");
 
+// select the value of from currency
+
 let fromCurr = document.querySelector(".from-dropdown select");
+
+// select the value of to currency
 
 let toCurr = document.querySelector(".to-dropdown select");
 
@@ -69,7 +77,65 @@ const updateFlag = (element) => {
 
 }
 
+submit.addEventListener("click", (evt) => {
+    evt.preventDefault();
+  
+    let fromCurrency = fromCurr.value;
+    let toCurrency = toCurr.value;
+  
+    let URL = `${baseURL}/${fromCurrency.toLowerCase()}.json`;
+  
+    fetch(URL)
+
+      .then(res => res.json())
+
+      .then(data => {
+
+        let rate = data[fromCurrency.toLowerCase()][toCurrency.toLowerCase()] * inputAmount.value;
+
+        if (inputAmount.value >= 1111){
+
+            formContainer.style.width = "25rem";
+
+        }if (inputAmount.value >= 11111111){
+
+            formContainer.style.width = "30rem";
+
+        }if (inputAmount.value >= 11111111111111){
+
+            formContainer.style.width = "35rem";
+
+        }
+
+        if(inputAmount.value == ""){
+
+            result.innerText = "Enter the Amount";
+
+        }else{
+
+        result.innerText = `${inputAmount.value} ${fromCurrency.toUpperCase()} = ${rate} ${toCurrency.toUpperCase()}`
+
+        }
+
+      })
+
+      .catch(err => console.error("Error fetching data:", err));
 
 
+  });
+  
 
+  document.addEventListener("DOMContentLoaded", () => {
+
+    inputAmount.addEventListener("input", () => {
+
+        if (inputAmount.value.trim() === "") {
+
+            formContainer.style.width = "20rem";
+
+            result.innerText = "Enter the Amount";
+
+        }
+    });
+});
 
